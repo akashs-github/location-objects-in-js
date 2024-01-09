814bb78d69e4f496366cc4a0ed4de40d8102159b
Akash <155422827+akashs-github@users.noreply.github.com>
2024-01-09 11:45:29 +0530

    Add files via upload
    Practicing Bom objects

---
+++ bom-location.js
@@ -0,0 +1,12 @@
+console.log("window location=",window.location)
+console.log("window herf=",window.location.href)
+console.log("hostname=",window.location.hostname)
+console.log("host=",window.location.host)
+console.log("pathnane=",window.location.pathname)
+console.log("protocol=",window.location.protocol)
+
+
+function open_google(){
+    window.location.assign("https://www.google.co.in/?hl=hi")
+}
+
